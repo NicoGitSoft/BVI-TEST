8a6015af45d099aa14075377a9ad9ee01bdf4a71
nicolas.ibanez.r@usach.cl <nicolas.ibanez.r@usach.cl>
2023-02-20 04:12:12 -0300

    .

--- Scripts/asdfasdf.py
+++ Scripts/asdfasdf.py
@@ -14,15 +14,6 @@ def Vertices(detection):
 ##################### inicialización de variables #####################
 times = []  # Muestras de los tiempos de ejecución del programa desde la primera captura de frame
 fps = []    # Muestras de los fps de ejecución del programa desde la primera captura de frame
-d = []      # Muestras de la distancias a la detecion más cercana con visión monocular
-z = []      # Muestras de las distancias a los obstáculos en la ROI central con visión estereoscópica
-h = []      # Muestras de las distancias horizontales en piexeles de la detección más cercana al centro de la imagen
-v = []      # Muestras de las distancias verticales en piexeles de la detección más cercana al centro de la imagen 
-
-# Muestras de los mensajes de activación de la interfaz de comunicación
-haptic_messages  = []   # Muestras de los mensajes de activación del vibrador UP de la interfaz háptica
-buzzer_messages  = []   # Muestras de los mensajes de activación del buzzer
-nearest_labels = []     # Muestras de la etiquta de la deteción más cercana
 
 # Muestras de temperatura
 chipTemperatures = []       # Muestras de la temperatura del chip
@@ -30,30 +21,36 @@ cpuTemperature = []         # Muestras de la temperatura del CPU
 max6675Temperature = []     # Muestras de la temperatura del sensor DHT22
 
 # Declaración de variables
-mentioned_object = False    # bandera booleana para evitar que se repita el nombre del objeto detectado
-mentioned_hand_use = False  # bandera booleana para evitar que se repita el la mencion TTS del uso de la mano
 frame_start_time = 0        # tiempo de inicio de captura de frame
 frames_timer = 0            # tiempo de captura de frame
 frames_counter = 0          # contador de frames
-start_move_time = 0         # tiempo de inicio de movimiento
-start_buzze_time = 0        # tiempo de inicio de sonido del buzzer
 
 # Rutas de los modelos YOLOv8n, YOLOv7t, YOLOv7s, YOLOv5n
 SCRIPT_DIR = Path(__file__).resolve().parent
-SingsYOLOv8n_MODEL = str(SCRIPT_DIR / "../Models/Sings/YOLOv8n/YOLOv8n_openvino_2021.4_6shave.blob")
-YOLOv8n_MODEL = str(SCRIPT_DIR / "../Models/Sings/SingsYOLOv8n/SingsYOLOv8n_openvino_2021.4_6shave.blob")
+
+SingsYOLOv8n_MODEL = str(SCRIPT_DIR / "../Models/Sings/SingsYOLOv8n/SingsYOLOv8n_openvino_2021.4_6shave.blob")
 SingsYOLOv7s_MODEL = str(SCRIPT_DIR / "../Models/Sings/SingsYOLOv7s/SingsYOLOv7s_openvino_2021.4_6shave.blob")
 SingsYOLOv7t_MODEL = str(SCRIPT_DIR / "../Models/Sings/SingsYOLOv7t/SingsYOLOv7t_openvino_2021.4_6shave.blob")
 SingsYOLOv5n_MODEL = str(SCRIPT_DIR / "../Models/Sings/SingsYOLOv5n/SingsYOLOv5n_openvino_2021.4_6shave.blob")
+YOLOv8n_MODEL = str(SCRIPT_DIR / "../Models/YOLO/YOLOv8n/YOLOv8n_openvino_2021.4_6shave.blob")
+YOLOv7s_MODEL = str(SCRIPT_DIR / "../Models/YOLO/YOLOv7s/YOLOv7s_openvino_2021.4_6shave.blob")
+YOLOv7t_MODEL = str(SCRIPT_DIR / "../Models/YOLO/YOLOv7t/YOLOv7t_openvino_2021.4_6shave.blob")
+YOLOv5n_MODEL = str(SCRIPT_DIR / "../Models/YOLO/YOLOv5n/YOLOv5n_openvino_2021.4_6shave.blob")
+
+
 SingsYOLOv8n_CONFIG = str(SCRIPT_DIR / "../Models/Sings/SingsYOLOv8n/SingsYOLOv8n.json")
-YOLOv8n_CONFIG = str(SCRIPT_DIR / "../Models/Sings/YOLOv8n/YOLOv8n.json")
 SingsYOLOv7t_CONFIG = str(SCRIPT_DIR / "../Models/Sings/SingsYOLOv7t/SingsYOLOv7t.json")
 SingsYOLOv7s_CONFIG = str(SCRIPT_DIR / "../Models/Sings/SingsYOLOv7s/SingsYOLOv7s.json")
 SingsYOLOv5n_CONFIG = str(SCRIPT_DIR / "../Models/Sings/SingsYOLOv5n/SingsYOLOv5n.json")
 
+
+
+YOLOv8n_CONFIG = str(SCRIPT_DIR / "../Models/YOLO/YOLOv8n/YOLOv8n.json")
+
+
 # Listas de los modelos y sus respectivas configuraciones
-SingsYOLO_MODELS = [SingsYOLOv8n_MODEL, SingsYOLOv7t_MODEL, SingsYOLOv5n_MODEL] 
-SingsYOLO_CONFIGS = [SingsYOLOv8n_CONFIG, SingsYOLOv7t_CONFIG, SingsYOLOv5n_CONFIG]
+SingsYOLO_MODELS = [YOLOv8n_MODEL, SingsYOLOv7t_MODEL, SingsYOLOv5n_MODEL] 
+SingsYOLO_CONFIGS = [YOLOv8n_CONFIG, SingsYOLOv7t_CONFIG, SingsYOLOv5n_CONFIG]
 
 visualize = True
 max_frames = 1000
